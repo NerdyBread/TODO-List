e215179476b65adb177e875011b7a812c41fdc05
Sam F <samfondacaro2006@gmail.com>
2021-06-16 15:48:01 -0400

    Small changes to logout

--- app/routes.py
+++ app/routes.py
@@ -56,7 +56,7 @@ def login():
 @app.route('/logout')
 def logout():
 	logout_user()
-	return redirect(url_for('login'))
+	return redirect(url_for('index'))
 
 @app.route('/signUp', methods=['GET', 'POST'])
 def signUp():
